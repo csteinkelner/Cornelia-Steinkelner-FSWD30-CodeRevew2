a4738499da6c8e75835b08bb44c8bb0810271ad6
Cornalia Steinkelner <cornelia.steinkelner@chello.at>
2017-12-01 17:40:39 +0100

    finished product

---
+++ new.js
@@ -0,0 +1,27 @@
+Amsterdam = [1500, 500, 1800, 700];
+
+London = [2500, 1500, 1000, 900];
+
+
+var avAm= (Amsterdam[0]+Amsterdam[1]+Amsterdam[2]+Amsterdam[3]) /4;
+
+var avLo= (London[0]+London[1]+London[2]+London[3])/4;
+
+
+var dif = avLo - avAm;
+document.getElementById("differ").innerHTML = dif;
+
+function city_1(city_name, country,average_price){
+	document.getElementById("city1").innerHTML = city_name;
+	document.getElementById("Country1").innerHTML = country;
+	document.getElementById("average1").innerHTML = average_price;
+};
+city_1("Amsterdam","Netherlands", avAm);
+
+function city_2(city_name, country,average_price){
+	document.getElementById("city2").innerHTML = city_name;
+	document.getElementById("Country2").innerHTML = country;
+	document.getElementById("average2").innerHTML = average_price;
+};
+city_2("London","United Kingdom", avLo);
+
