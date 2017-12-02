7e9fdb5bab70201481fce88bb8759e0d85604938
Cornalia Steinkelner <cornelia.steinkelner@chello.at>
2017-12-02 13:34:06 +0100

    forgot the last change push

--- new.js
+++ new.js
@@ -15,6 +15,7 @@ function city_1(city_name, country,average_price){
 	document.getElementById("city1").innerHTML = city_name;
 	document.getElementById("Country1").innerHTML = country;
 	document.getElementById("average1").innerHTML = average_price;
+	document.getElementById("city_1").innerHTML = city_name;
 };
 city_1("Amsterdam","Netherlands", avAm);
 
@@ -22,6 +23,7 @@ function city_2(city_name, country,average_price){
 	document.getElementById("city2").innerHTML = city_name;
 	document.getElementById("Country2").innerHTML = country;
 	document.getElementById("average2").innerHTML = average_price;
+	document.getElementById("city_2").innerHTML = city_name;
 };
 city_2("London","United Kingdom", avLo);
 
